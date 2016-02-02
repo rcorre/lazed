72f7cd6efe929d5ec67833fa3ec12adf55f197ff
Ryan Roden-Corrent <ryan@rcorre.net>
2016-02-01 21:38:34 -0500

    2D ray/ray and ray/segment intersection functions.

--- src/geometry.d
+++ src/geometry.d
@@ -15,53 +15,110 @@ import gfm.math;
  * The point of intersection,
  */
 auto intersect(ray2f a, ray2f b) {
+    // algorithm from stack overflow answer:
+    // http://stackoverflow.com/a/2932601/1435461
+    immutable as = a.orig,
+              bs = b.orig,
+              ad = a.dir,
+              bd = b.dir,
+              dx = bs.x - as.x,
+              dy = bs.y - as.y,
+              det = bd.x * ad.y - bd.y * ad.x,
+              u = (dy * bd.x - dx * bd.y) / det,
+              v = (dy * ad.x - dx * ad.y) / det;
 
-    /*
-     * y - ya = ma(x - xa)
-     * y - yb = mb(x - xb)
-     *--------------------
-     * -ya + yb = max - maxa - mbx + mbxb
-     * yb - ya = x(ma - mb) + mbxb - maxa
-     * yb - ya - mbxb + maxa = x(ma - mb)
-     * x = (yb - ya + maxa - mbxb) / (ma - mb)
-     *
-     * ...
-     *
-     * y = ma(x - xa) + ya
-     */
-
-    immutable xa = a.orig.x, // sample x point on a
-              xb = b.orig.x, // sample x point on b
-              ya = a.orig.y, // sample y point on a
-              yb = b.orig.y, // sample y point on b
-              ma = a.dir,    // slope of a
-              mb = b.dir;    // slope of b
-
-    if (ma == mb) { // parallel -- check if they are the same ray
-        auto disp = b.orig - a.orig;
-        auto slope = disp.y / disp.x;
-        if (a.orig - b.orig
-        return (a.orig == b.orig) ? intersection(a.orig) : noIntersection;
-    }
-
-
-    immutable x = yb - ya + ma*xa + mb*xb / (ma - mb);
-    immutable y = ma*(x - xa) + ya;
-
-    // ensure (x,y) lies on both of the rays
-    return ((x > xa && ma > 0 || x < xa && ma < 0) &&
-            (x > xb && mb > 0 || x < xb && mb < 0)) ?
-        intersection(vec2f(x, y)) : noIntersection;
+    return (u > 0 && v > 0) ? intersection(a.progress(u)) : noIntersection;
 }
 
 unittest {
-    bool test(int[2] o1, int[2] m1, int[2] o2, int[2] m2, ) {
+    // Validate that the ray (as, ad) intersects the ray (bs, bd) at p
+    bool yes(float[2] as, float[2] ad, float[2] bs, float[2] bd, float[2] p) {
+        import std.math : approxEqual;
+
+        auto a = ray2f(vec2f(as), vec2f(ad));
+        auto b = ray2f(vec2f(bs), vec2f(bd));
+        auto res = intersect(a, b);
+
+        return res && res.x.approxEqual(p[0]) && res.y.approxEqual(p[1]);
     }
 
-    assert((pt = ray2i(
+    // Validate that the ray (as, ad) doesn't intersect the ray (bs, bd)
+    bool no(float[2] as, float[2] ad, float[2] bs, float[2] bd) {
+        auto r1 = ray2f(vec2f(as), vec2f(ad));
+        auto r2 = ray2f(vec2f(bs), vec2f(bd));
+        return !intersect(r1, r2);
+    }
+
+    //            as        ad        bs        bd        p
+    assert(yes([ 0, 0 ], [ 1, 1 ], [ 0, 2 ], [ 1,-1 ], [ 1, 1 ]));
+    assert(yes([ 0, 0 ], [ 4, 4 ], [ 0, 2 ], [ 8,-8 ], [ 1, 1 ]));
+    assert(yes([ 0, 0 ], [ 2, 1 ], [ 4, 0 ], [ 0, 1 ], [ 4, 2 ]));
+    assert(yes([ 0, 0 ], [ 2, 1 ], [ 3, 1 ], [ 1, 1 ], [ 4, 2 ]));
+    assert(yes([ 0, 0 ], [ 2, 1 ], [ 5, 1 ], [-1, 1 ], [ 4, 2 ]));
+    assert(yes([ 0, 0 ], [ 2, 1 ], [ 4, 5 ], [ 0,-1 ], [ 4, 2 ]));
+
+    //            as        ad        bs        bd
+    assert(no([ 0, 0 ], [ 2, 1 ], [-1,-1 ], [ 0, 1 ]));
+    assert(no([ 0, 0 ], [-1,-1 ], [ 1, 1 ], [ 1, 1 ]));
+    assert(no([ 0, 0 ], [-1,-1 ], [ 1, 1 ], [ 1, 0 ]));
+    assert(no([ 0, 0 ], [-1,-1 ], [ 1, 1 ], [ 0, 1 ]));
+    assert(no([ 0, 0 ], [ 1, 0 ], [ 1, 0 ], [ 1, 0 ])); // parallel
 }
 
-bool intersect(ray2f ray, box2f box, out vec2f point) {
+/**
+ * Determine if and where two rays intersect.
+ *
+ * If the rays are identical, returns the first point they meet.
+ *
+ * Params:
+ * a = first ray
+ * b = second ray
+ *
+ * Returns:
+ * The point of intersection,
+ */
+auto intersect(ray2f a, seg2f b) {
+    // algorithm from stack overflow answer:
+    // http://stackoverflow.com/a/2932601/1435461
+    immutable as = a.orig,
+              bs = b.a,
+              ad = a.dir,
+              bd = b.b - b.a,
+              dx = bs.x - as.x,
+              dy = bs.y - as.y,
+              det = bd.x * ad.y - bd.y * ad.x,
+              u = (dy * bd.x - dx * bd.y) / det,
+              v = (dy * ad.x - dx * ad.y) / det;
+
+    return (u > 0 && v > 0 && v < 1) ? intersection(a.progress(u)) : noIntersection;
+}
+
+unittest {
+    // Validate that the ray (as, ad) intersects the segment (ba, bb) at p
+    bool yes(float[2] as, float[2] ad, float[2] ba, float[2] bb, float[2] p) {
+        import std.math : approxEqual;
+
+        auto a = ray2f(vec2f(as), vec2f(ad));
+        auto b = seg2f(vec2f(ba), vec2f(bb));
+        auto res = intersect(a, b);
+
+        return res && res.x.approxEqual(p[0]) && res.y.approxEqual(p[1]);
+    }
+
+    // Validate that the ray (as, ad) doesn't intersect the segment (ba, bb)
+    bool no(float[2] as, float[2] ad, float[2] ba, float[2] bb) {
+        auto a = ray2f(vec2f(as), vec2f(ad));
+        auto b = seg2f(vec2f(ba), vec2f(bb));
+        return !intersect(a, b);
+    }
+
+    //            as        ad        ba        bb        p
+    assert(yes([ 0, 0 ], [ 1, 1 ], [ 4, 0 ], [ 0, 4 ], [ 2, 2 ]));
+    assert(yes([ 0, 0 ], [-4, 4 ], [-2, 0 ], [ 0, 2 ], [-1, 1 ]));
+
+    //            as        ad        ba        bb
+    assert(no([ 0, 0 ], [ 1, 1 ], [ 2, 1 ], [ 4, 1 ]));
+    assert(no([ 0, 0 ], [-1,-1 ], [ 1, 1 ], [ 2, 2 ]));
 }
 
 private:
